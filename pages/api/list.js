30b73134e07551241176641ee78a0d49c0fd6dc1
Hiroyuki Watanabe <92242109+hiro-shiawaido@users.noreply.github.com>
2023-06-04 10:54:30 +0900

    v2

---
+++ pages/api/list.js
@@ -0,0 +1,16 @@
+import { connectToDatabase } from "../../lib/mongodb";
+
+export default async function handler(request, response) {
+    
+    const { database } = await connectToDatabase();
+    const collection = database.collection(process.env.NEXT_ATLAS_COLLECTION);
+
+    const results = await collection.find({})
+    .project({
+            
+    })
+    .limit(3).toArray();
+
+    response.status(200).json(results);
+
+}
